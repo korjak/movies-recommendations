cef070c1f6c9256589de3e6951ee0ca8f4782cdc
Jakub Korthals <korthalsjakub@gmail.com>
2019-07-02 14:23:09 +0200

    Add user panel

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
 
 app = Flask(__name__)
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="toor",
+    database="moviesratings"
+)
+mycursor = mydb.cursor()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -8,11 +16,43 @@ def login():
     if request.method == 'GET':
         return render_template("index.html")
     else:
-        return user_home()
+        return redirect(url_for('user_home', username=request.form['username']))
 
-@app.route('/user/<username>')
-def user_home():
+
+@app.route('/user/<username>', methods=['GET', 'POST'])
+def user_home(username):
+    mycursor.execute('SELECT * FROM Users WHERE name="' + username + '"')
+    data = mycursor.fetchall()
+    if request.method == 'GET':
+        if len(data) == 0:
+            return "no user"
+        else:
+            return render_template("user_panel.html", username=username)
+    else:
+        if request.form['choose_action'] == 'rate':
+            return 'rate'
+        elif request.form['choose_action'] == 'recommend':
+            return 'recommend'
+        else:
+            return "something went wrong"
+
+
+@app.route('/user/<username>/rate')
+def give_rates(username):
     pass
+
+
+@app.route('/test/<username>', methods=['GET', 'POST'])
+def tester(username):
+    if request.method == 'GET':
+        return render_template("user_panel.html", username=username)
+    else:
+        if request.form['choose_action'] == 'rate':
+            return redirect(url_for('give_rates'), username=username)
+        elif request.form['choose_action'] == 'recommend':
+            return redirect(url_for('get_recommendations'), username=username)
+        else:
+            return "something went wrong"
 
 
 if __name__ == '__main__':
